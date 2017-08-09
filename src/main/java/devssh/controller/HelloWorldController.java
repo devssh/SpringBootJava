343e6050d8d58d447fd64777d7c6a73a77a0f928
Devashish Sood <devasood@gmail.com>
2017-08-09 18:38:25 +0530

    Added logging

--- src/main/java/devssh/controller/HelloWorldController.java
+++ src/main/java/devssh/controller/HelloWorldController.java
@@ -1,6 +1,7 @@
 package devssh.controller;
 
 import devssh.service.HelloWorldService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,13 +10,15 @@ import javax.ws.rs.core.MediaType;
 
 @RestController
 @Produces(MediaType.APPLICATION_JSON)
-@RequestMapping("/hello-world")
+@Slf4j
 public class HelloWorldController {
     @Autowired
     HelloWorldService helloWorldService;
 
     @RequestMapping(value = "/java", method = RequestMethod.GET, produces = "application/json")
-    public String sayHello(@RequestParam("name") String name) {
+    public String sayHello() {
+        String name="dave";
         return helloWorldService.sayHelloWorld(name);
     }
+
 }
